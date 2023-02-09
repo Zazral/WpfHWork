60b5addc0cc4b92d7d7e5646fff27b62067082c6
ИК <@DESKTOP-IVMPSKR>
2023-02-09 17:31:31 +0300

    Добавьте файлы проекта.

---
+++ WpfHWork/Window1.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfHWork
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow MaWin = new MainWindow();
+            MaWin.Show();
+            Close();
+        }
+
+        private void TextBox_MouseEnter(object sender, MouseEventArgs e)
+        {
+            txtbox.Clear();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string[] str;
+            str = txtbox.Text.Split(' ');
+            lstbox.Items.Clear();
+            foreach (string el in str)
+            {
+                lstbox.Items.Add(el);
+            }
+        }
+    }
+}
